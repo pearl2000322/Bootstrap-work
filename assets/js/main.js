96dc3393bb4866e4003f6416e6e85d2994f4004f
pearl2000322 <pearl200322@gmail.com>
2022-02-06 00:53:11 +0700

    done
    initial

---
+++ assets/js/main.js
@@ -0,0 +1,29 @@
+function showtext1() {
+    var dots = document.getElementById("dots");
+    var moreText = document.getElementById("more");
+    var btnText = document.getElementById("myBtn");
+    if (dots.style.display == "none") {
+        dots.style.display = "inline";
+        btnText.innerHTML = "...more";
+        moreText.style.display = "none";
+    } else {
+        dots.style.display = "none";
+        btnText.innerHTML = "...less";
+        moreText.style.display = "inline";
+    }
+}
+
+function showtext2() {
+    var dots = document.getElementById("dots1");
+    var moreText = document.getElementById("more1");
+    var btnText = document.getElementById("myBtn1");
+    if (dots.style.display == "none") {
+        dots.style.display = "inline";
+        btnText.innerHTML = "...more";
+        moreText.style.display = "none";
+    } else {
+        dots.style.display = "none";
+        btnText.innerHTML = "...less";
+        moreText.style.display = "inline";
+    }
+}
